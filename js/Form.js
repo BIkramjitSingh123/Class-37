8bee0ce6636dce9a907598714579870205a9f682
BIkramjitSingh123 <72287543+BIkramjitSingh123@users.noreply.github.com>
2021-01-15 20:05:03 -0500

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,42 @@
+class Form{
+    constructor(){
+        this.input = createInput("name");
+        this.button = createButton("play");
+        this.greeting = createElement("h2");
+    }
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+    }
+    display(){
+        var title = createElement("h2");
+        title.html("Car Racing Game");
+        title.position(130,0);
+
+        // var input = createInput("name");
+        // var button = createButton("play");
+        // var greeting = createElement("h3");
+
+        this.input.position(130,160);
+        this.button.position(250,200);
+        
+        this.button.mousePressed(() =>{
+            this.input.hide();
+            this.button.hide();
+
+            var name = this.input.value();
+            playerCount = playerCount + 1;
+            player.update(name)
+            player.updateCount(playerCount);
+
+            this.greeting.html("Welcome: " + name);
+            this.greeting.position(130,50);
+
+            
+        })
+
+    }
+
+
+}
